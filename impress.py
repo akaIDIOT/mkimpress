1aa2e2ca67bcab5b392d7d4785fd43211520a4c3
Mattijs Ugen <akaidiot@users.noreply.github.com>
2017-09-23 13:33:11 +0200

    Move project into single file, add main() and make()

--- impress.py
+++ impress.py
@@ -1,7 +1,8 @@
+import argparse
+import re
+
 from jinja2 import Environment, FileSystemLoader
 from markdown import markdown
-
-from impress import SLIDE_DELIMITER
 
 
 class Slide:
@@ -23,7 +24,7 @@ class Slide:
         return '<Slide {}>'.format(self.num)
 
 
-def split_slides(content, delimiter=SLIDE_DELIMITER, strip=True):
+def split_slides(content, delimiter=re.compile(r'(?:\r?\n){3,}'), strip=True):
     split = delimiter.split(content)
     slides = []
 
@@ -36,7 +37,7 @@ def split_slides(content, delimiter=SLIDE_DELIMITER, strip=True):
 
 def render(slides, template_dir='template', **kwargs):
     env = Environment(loader=FileSystemLoader(template_dir))
-    template = env.get_template('presentation.html')
+    template = env.get_template('index.html')
 
     # TODO: add some config object with settings like transition duration with defaults, overridable from kwargs
     template_vars = {
@@ -46,3 +47,31 @@ def render(slides, template_dir='template', **kwargs):
     template_vars.update(kwargs)
 
     return template.render(**template_vars)
+
+
+def make(infile, outfile, template_dir='template'):
+    with open(infile, 'rt') as infile:
+        content = infile.read()
+
+    slides = split_slides(content)
+    slides = render(slides, template_dir=template_dir)
+
+    with open(outfile, 'wt') as outfile:
+        outfile.write(slides)
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('infile', metavar='INFILE')
+    parser.add_argument('outfile', metavar='OUTFILE')
+
+    parser.add_argument('-t', '--template-dir', default='template')
+
+    args = parser.parse_args(args)
+
+    make(args.infile, args.outfile, template_dir=args.template_dir)
+
+
+if __name__ == '__main__':
+    main()
--- impress/__init__.py
+++
@@ -1,4 +0,0 @@
-import re
-
-
-SLIDE_DELIMITER = re.compile(r'(?:\r?\n){3,}')
